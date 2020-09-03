793ef820433a427e250255c429506acbbde42c8b
Andrew Meshcheryakov <cryptonov@gmail.com>
2020-09-04 00:50:48 +0300

    Input component added

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,34 @@
+import React, { FC, InputHTMLAttributes } from 'react';
+// utils
+import cn from 'classnames';
+// styles
+import './Input.scss';
+
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
+  errorMessage?: String;
+}
+
+const Input: FC<InputProps> = ({
+  onChange,
+  errorMessage,
+  className,
+  value,
+  ...rest
+}) => (
+  <div
+    className={cn(
+      'input',
+      errorMessage && 'input_with-error',
+      className,
+    )}
+  >
+    <input className="input__field-input" {...rest} />
+    {errorMessage && (
+      <span className="input__error-message">
+        {errorMessage}
+      </span>
+    )}
+  </div>
+);
+
+export default Input;
